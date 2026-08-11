332bbbc55076b16b1d3468750b2544783d402cd4
FUAD-2501069 <2501069@student.ruet.ac.bd>
2026-08-11 22:20:28 +0600

    Add files via upload

---
+++ fff.c
@@ -0,0 +1,212 @@
+//Right Half Pyramid
+#include<stdio.h>
+int main(){
+int i,j,r;
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=1;j<=i;j++){
+        printf("*");
+    }printf("\n");
+}
+}
+
+//Right-Aligned Number Triangle
+# include<stdio.h>
+int main(){
+int i,j,r;
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=i;j++){
+    printf("%d",i);
+    }
+    printf("\n");
+}
+}
+
+//Full/Centered Pyramid
+# include<stdio.h>
+int main(){
+int i,j,r;
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=2*i-1;j++){
+    printf("*");
+    }
+    printf("\n");
+}
+}
+
+//Continuous Number Pyramid
+# include<stdio.h>
+int main(){
+int i,j,r,num=1;
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=i;j++){
+    printf(" %d",num);
+    num++;
+    }
+    printf("\n");
+}
+}
+
+//Inverted Right-Aligned Triangle
+# include<stdio.h>
+int main(){
+int i,j,r;
+scanf("%d",&r);
+for(i=r;i>=1;i--){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=i;j++){
+    printf("  *");
+    }
+    printf("\n");
+}
+}
+
+//Diamond(Rhombus)Pattern
+# include<stdio.h>
+int main(){
+int i,j,r;
+scanf("%d",&r);
+for(i=r;i>=1;i--){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=2*i-1;j++){
+    printf("*");
+    }
+    printf("\n");
+}for(i=2;i<=r;i++){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=2*i-1;j++){
+    printf("*");
+    }
+    printf("\n");
+
+}
+}
+
+//Solid Square
+#include<stdio.h>
+int main(){
+int i,j,r;
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=1;j<=r;j++){
+        printf("*");
+    }printf("\n");
+}
+}
+
+//Hollow Square
+#include<stdio.h>
+int main(){
+int i,j,r;
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=1;j<=r;j++){
+            if(i==1||i==r||j==1||j==r)
+        printf("*");
+    else
+        printf(" ");
+    }
+printf("\n");
+}
+}
+
+//Hollow Pyramid(Triangle Outline)
+# include<stdio.h>
+int main(){
+int i,j,r;
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=2*i-1;j++){
+       if(i==1||i==r||j==1||j==2*i-1)
+    printf("*");
+    else
+        printf(" ");
+    }
+    printf("\n");
+}
+}
+
+//Alphabet Pyramid(Same Letter per Row)
+# include<stdio.h>
+int main(){
+int i,j,r;
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=2*i-1;j++){
+    printf("%c",'A'+i-1);
+    }
+    printf("\n");
+}
+}
+
+//Continuous Alphabet Pyramid
+# include<stdio.h>
+int main(){
+int i,j,r;
+char n='A';
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=2*i-1;j++){
+    printf("%c",n);
+    n++;
+    }
+    printf("\n");
+}
+}
+
+//Left-Leaning Parallelogram
+# include<stdio.h>
+int main(){
+int i,j,r;
+
+scanf("%d",&r);
+for(i=1;i<=r;i++){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=r;j++){
+    printf("*");
+
+    }
+    printf("\n");
+}
+}
+
+//Right-Leaning Parallelogram
+# include<stdio.h>
+int main(){
+int i,j,r;
+
+scanf("%d",&r);
+for(i=r;i>=1;i--){
+    for(j=i;j<r;j++){
+        printf(" ");
+    } for(j=1;j<=r;j++){
+    printf("*");
+
+    }
+    printf("\n");
+}
+}
+
+
+
+
+
